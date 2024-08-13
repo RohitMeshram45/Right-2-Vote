3229baecb3bf73c42a7a7a2e31b8c275187b33fa
Rohit Meshram <115627734+rohit20220000@users.noreply.github.com>
2024-08-14 01:13:41 +0530

    modified the backend

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express();
-const db = require('./db'); 
-require('dotenv').config(); 
-const cors= require("cors")
-const bodyParser = require('body-parser');   
+const db = require('./db');
+require('dotenv').config();
+const cors = require("cors")
+const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // req.body 
-const PORT = process.env.REACT_APP_PORT; 
+const PORT = process.env.REACT_APP_PORT;
 
 const cookieParser = require('cookie-parser');
 
@@ -15,10 +15,21 @@ const userRoutes = require('./routes/userRoutes');
 const candidateRoutes = require('./routes/candidateRoutes');
 const contactRoutes = require('./routes/contactRoutes');
 
-app.use(cors({
-    origin: process.env.REACT_APP_FRONTEND_CONNECTTION, // Your frontend URL
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-}));
+const allowedOrigins = [
+    'https://voting-rohit.vercel.app',
+    'https://voting-rohit-nl2wyor2i-rohitmeshram45s-projects.vercel.app'
+];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+};
+// 
 // Use the routers
 
 app.use(express.static(path.join(__dirname, 'frontend/build')));
@@ -30,10 +41,12 @@ app.use('/user', userRoutes);
 app.use('/candidate', candidateRoutes);
 app.use('/contact', contactRoutes);
 
+app.use(cors(corsOptions));
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
-  });
+});
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     // console.log('listening on port ',PORT);
 })
