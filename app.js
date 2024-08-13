e7109975f1208c8ca2d07452f2eca6acda6cadd6
Rohit Jagdish Meshram <rohitjmeshram2003@gmail.com>
2024-08-14 00:25:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,29 +16,15 @@ const candidateRoutes = require('./routes/candidateRoutes');
 const contactRoutes = require('./routes/contactRoutes');
 
 
-const allowedOrigins = ["https://voting-rohit.vercel.app/"];
+ 
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    // Check if the origin is in the allowedOrigins array
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true, // if your frontend needs to send or receive cookies
-};
+app.use(cors({
+    origin: process.env.REACT_APP_FRONTEND_CONNECTTION, // Your frontend URL
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+}));
 
-
-// app.use(cors({
-//     origin: process.env.REACT_APP_FRONTEND_CONNECTTION, // Your frontend URL
-//     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-// }));
 // Use the routers
 
-// Use the CORS middleware
-app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
