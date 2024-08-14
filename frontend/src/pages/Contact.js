0abf82538683bea0bffbc9b06e6ac35c581eab61
Rohit Jagdish Meshram <rohitjmeshram2003@gmail.com>
2024-08-14 22:45:37 +0530

    Update Contact.js
    Correct Form input and onChange button

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -50,17 +50,17 @@ const Contact = () => {
             <p class="leading-relaxed mb-5 text-gray-200 font-bold text-center">Fee Free to Contact with me</p>
             <div class="relative mb-4">
               <label for="name" class="leading-7 text-sm text-gray-200 font-bold">Name : </label>
-              <input value={name} onChange={(e) => setName} placeholder='Enter the Name' type="text" id="name" name="name" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+              <input value={name} onChange={(e) => setName(e.target.value)} placeholder='Enter the Name' type="text" id="name" name="name" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
             </div>
             <div class="relative mb-4">
               <label for="email" class="leading-7 text-sm text-gray-200 font-bold">Email : </label>
-              <input value={email} onChange={(e) => setEmail} placeholder='Enter the Email' type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+              <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Enter the Email' type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
             </div>
             <div class="relative mb-4">
               <label for="message" class="leading-7 text-sm text-gray-200 font-bold">Message : </label>
               <textarea value={massege} onChange={(e) => setMassege(e.target.value)} placeholder='Enter the Message' id="message" name="message" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
             </div>
-            <button onClick={() => handleContact} class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg font-sans">Button</button>
+            <button onClick={handleContact} class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg font-sans">Button</button>
           </div>
         </div>
       </section>
